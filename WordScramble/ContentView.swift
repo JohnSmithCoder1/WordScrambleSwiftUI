38180bb97ac5d22cf1715c861901af8203af66f0
John Smith <johnsmithcoder1@gmail.com>
2022-12-30 15:41:11 -0600

    Add validation functions

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -57,6 +57,32 @@ struct ContentView: View {
         
         fatalError("Could not load start.txt from bundle.")
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let i = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: i)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
